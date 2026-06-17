5c6f852df0eebf9b8daf7c878e09fcae0ae5cfe8
Kelvin Ojeamiren <kelvin.ojeamiren.dev@gmail.com>
2026-06-17 14:14:01 +0100

    bout to work on typing engine

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ import { startTestOverlay } from "./remove-overlay.js";
 import { splitAppData,spans } from "./source-of-truth.js";
 import { displayArea } from "./display-and-input-area.js";
 
+//IMPORTING COUNTERS FROM TYPING ENGINE
+import { totalChars } from "./typing-engine.js";
 
 //APP STATE DECLARATION AND REUSABLE FUNCTION
 let appState = "notStarted";
---
+++ scripts/typing-engine.js
@@ -0,0 +1,19 @@
+import { spans } from "./source-of-truth.js";
+import { displayArea,userInput } from "./display-and-input-area.js";
+
+//DECLARING COUNTERS
+let currentIndex = 0;
+let correctChars = 0;
+let incorrectChars = 0;
+let totalChars = spans.length;
+let highlightIndex = 0;
+
+
+
+//TYPING ENGINE;
+
+
+
+
+
+export {totalChars};
